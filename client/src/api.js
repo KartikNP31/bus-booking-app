fba194057ed0de1bf797fe5fe4de3c3301058b30
KartikNP31 <patelkn1303@gmail.com>
2025-01-15 15:38:34 +0530

    Update PUBLIC_SERVER_URL to point to the Vercel deployment

--- client/src/api.js
+++ client/src/api.js
@@ -1,1 +1,2 @@
-export const PUBLIC_SERVER_URL="http://localhost:8000"
+export const PUBLIC_SERVER_URL="https://bus-booking-app-backend.vercel.app/"
+// export const PUBLIC_SERVER_URL="http://localhost:8000"
